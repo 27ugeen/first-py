c3b304d8df4de40c01ff826f2ab2196421e0a41a
27ugeen <27ugeen@gmail.com>
2023-11-11 21:49:37 +0200

    module1 - module2

--- main.py
+++ main.py
@@ -1,3 +1,154 @@
-print("Hello world")
+# print("Hello world")
 
-print("Hello Git")
+# print("Hello Git")
+
+# a = None
+
+# print(f"first a {a}")
+
+# a = "string"
+
+# print(f"second a {a}")
+
+# a = 5
+
+# print(f"third a {a}")
+
+
+# first_name = "Margo"
+# last_name = "Robbie"
+# full_name = f"{first_name} {last_name}"
+
+# a = -2 + 3j
+# b = 4 + 2.1j
+# result = a + b
+# print(result)
+
+# import math
+
+# a = -2
+# b = 7
+# c = -6
+# D = b
+# x1 =
+# x2 =
+
+
+# a = input('Введіть число')
+# a = int(a)
+# if a > 0:
+#     print('Число додатне')
+# elif a == 1:
+#     print('Число дорівнює 1')
+# else:
+#     print("a <= 0")
+
+# fruit = "apple"
+
+# for i in fruit:
+#     print(i)
+
+# a = 5
+
+# while a < 10:
+#     print(a)
+#     a += 1
+
+
+# while True:
+#     user_input = input("enter exit:")
+#     print(user_input)
+#     if user_input == "exit":
+#         break
+
+# while True:
+#     number = input("number = ")
+#     number = int(number)
+#     while True:
+#         print(number)
+#         number = number - 1
+#         if number < 0:
+#             break
+
+# val = 'abc'
+# try:
+#     val = int(val)
+# except ValueError:
+#     print(f"val {val} is not a number")
+# else:
+#     print(val > 0)
+# finally:
+#     print("This will be printed anyway")
+
+
+# def printNumber(a,b):
+#     if a > b:
+#         print("a is greater than b")
+#     elif a == b:
+#         print("a is equal b")
+#     else:
+#         print("a is less than b")
+
+# printNumber(2,4)
+# printNumber(2, 3.5)
+
+# def sum(a,b):
+#     return a**b
+
+# print(sum(2,3))
+
+# def factorial(n):
+#     if n <= 1:
+#         r = 1
+#     else:
+#         r = n * factorial(n - 1)  
+#     return r
+    
+# print(factorial(5))
+
+# import math
+
+# sinPi = math.sin(math.pi)
+# print(sinPi)
+
+# def say_hello(name):
+#     print(f'Hello {name}')
+
+
+
+# def main():
+#     print("You imported hello.py")
+#     say_hello('user')
+
+# # for arg in sys.argv:
+# #     print(arg)
+# if __name__ == '__main__':
+#     main()
+
+# num = int(input("Enter the integer (0 to 100): "))
+# sum = 0
+
+# while num > 0:
+#     sum += num
+#     num -= 1
+    
+# print(sum)
+
+# first = int(input("Enter the first integer: "))
+# second = int(input("Enter the second integer: "))
+
+# gcd = first if (first < second) else second
+
+# while first%gcd != 0 or second%gcd != 0:
+#     gcd -= 1
+#     print(gcd)
+
+# num = int(input("Enter integer (0 for output): "))
+# sum = 0
+# while num != 0:
+#     for el in range(num +1):
+#         sum += el   
+#     num = int(input("Enter integer (0 for output): "))
+
+# print(sum)
+
